1f8d6c387ae75ae9fce979b8a5d8f7255993df78
Steveb175 <123309239+Steveb175@users.noreply.github.com>
2024-04-01 17:14:57 +0000

    basic HTML started

---
+++ assets/index.js
@@ -0,0 +1,8 @@
+// Conversion of input values
+
+let startingInput = document.getElementById(startingInput);
+let convertedInput = document.getElementById(convertedInput);
+
+function convertTemp(startingInput, convertedInput) {
+
+}
