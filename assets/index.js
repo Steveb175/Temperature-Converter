44dd12ae86bb8100e23176aa6bb85a4e35660971
Steveb175 <123309239+Steveb175@users.noreply.github.com>
2024-04-01 19:41:57 +0000

    JS function possibly completed?

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,35 @@
 // Conversion of input values
+function convertTemp() {
+    const startingTemp = document.getElementById(startingTemp);
+    const startingUnit = document.getElementById(startingUnit);
+    const convertedUnit = document.getElementById(convertedUnit);
 
-let startingInput = document.getElementById(startingInput);
-let convertedInput = document.getElementById(convertedInput);
+    let convertedTemp;
 
-function convertTemp(startingInput, convertedInput) {
-
+    if (startingUnit === "fahrenheit") {
+        if (convertedUnit === "celsius") {
+            convertedTemp = (startingTemp - 32) * 5/9;
+        } else if (convertedUnit === "kelvin") {
+                convertedTemp = (startingTemp - 32) * (5/9) + 273.15;
+            } else {
+                startingTemp = convertedTemp;
+            }
+    } else if (startingUnit === "celsius") {
+        if (convertedUnit === "fahrenheit") {
+            convertedTemp = (startingTemp*(9/5));
+        } else if (convertedUnit === "kelvin") {
+            convertedTemp = (startingTemp + 273.15);
+        } else {
+            startingTemp = convertedTemp;
+        }
+    } else if (startingUnit === "kelvin") {
+        if (convertedUnit === "fahrenheit") {
+            convertedTemp = (9/5)*(startingTemp - 273) + 32;
+        } else if (convertedUnit === "celsius") {
+            convertedTemp = (startingTemp - 273.15);
+        } else {
+            startingTemp = convertedTemp;
+        }
+    }
+    document.getElementById("convertedTemp").value = convertTemp.toFixed(2);
 }
