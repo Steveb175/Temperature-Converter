46340af9fe3bd9c08e9491fbe94d374a08a059e7
Stephen Bondurant <stephenbondurant2014@gmail.com>
2024-04-01 21:39:59 -0500

    Round to 2 decimal places

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,8 @@
 // Conversion of input values
 function convertTemp() {
-  const startingTemp = document.getElementById("startingTemp").value;
+  const startingTemp = parseFloat(
+    document.getElementById("startingTemp").value
+  );
   const startingUnit = document.getElementById("startingUnit").value;
   const convertedUnit = document.getElementById("convertedUnit").value;
 
@@ -8,27 +10,27 @@ function convertTemp() {
 
   if (startingUnit === "fahrenheit") {
     if (convertedUnit === "celsius") {
-      convertedTemp = (startingTemp - 32) * (5 / 9);
+      convertedTemp = ((startingTemp - 32) * (5 / 9)).toFixed(2);
     } else if (convertedUnit === "kelvin") {
-      convertedTemp = (startingTemp - 32) * (5 / 9) + 273.15;
+      convertedTemp = ((startingTemp - 32) * (5 / 9) + 273.15).toFixed(2);
     } else {
-      convertedTemp = startingTemp;
+      convertedTemp = startingTemp.toFixed(2);
     }
   } else if (startingUnit === "celsius") {
     if (convertedUnit === "fahrenheit") {
-      convertedTemp = startingTemp * (9 / 5);
+      convertedTemp = (startingTemp * (9 / 5)).toFixed(2);
     } else if (convertedUnit === "kelvin") {
-      convertedTemp = startingTemp + 273.15;
+      convertedTemp = (startingTemp + 273.15).toFixed(2);
     } else {
-      convertedTemp = startingTemp;
+      convertedTemp = startingTemp.toFixed(2);
     }
   } else if (startingUnit === "kelvin") {
     if (convertedUnit === "fahrenheit") {
-      convertedTemp = (9 / 5) * (startingTemp - 273) + 32;
+      convertedTemp = ((9 / 5) * (startingTemp - 273) + 32).toFixed(2);
     } else if (convertedUnit === "celsius") {
-      convertedTemp = startingTemp - 273.15;
+      convertedTemp = (startingTemp - 273.15).toFixed(2);
     } else {
-      convertedTemp = startingTemp;
+      convertedTemp = startingTemp.toFixed(2);
     }
   }
   document.getElementById("convertedTemp").value = convertedTemp;
