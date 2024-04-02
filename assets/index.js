01261662fde833cb21068d4abc051fabac9edf86
Steveb175 <123309239+Steveb175@users.noreply.github.com>
2024-04-02 15:39:52 +0000

    completed color changes based on temp

--- assets/index.js
+++ assets/index.js
@@ -36,14 +36,47 @@ function convertTemp() {
   document.getElementById("convertedTemp").value = convertedTemp;
   //Call freezing function
   freezing(convertedUnit, convertedTemp);
+  normal(convertedUnit, convertedTemp);
+  hot(convertedUnit, convertedTemp);
 }
 
 function freezing(convertedUnit, convertedTemp) {
+  var mainContainer = document.querySelector(".main-container");
+  var btn = document.querySelector(".btn");
+
     if ((convertedUnit === "fahrenheit" && convertedTemp <= 32) ||
      (convertedUnit === "celsius" && convertedTemp <= 0) ||
      (convertedUnit === "kelvin" && convertedTemp <= 273.15)) {
      document.body.style.backgroundColor = "#6494b4";
+     mainContainer.style.backgroundColor = "#afd1f2";
+     btn.style.backgroundColor = "#92b4cc"
     }
+}
+
+function normal(convertedUnit, convertedTemp) {
+  var mainContainer = document.querySelector(".main-container");
+  var btn = document.querySelector(".btn");
+
+  if ((convertedUnit === "fahrenheit" && convertedTemp > 32 && convertedTemp < 80) ||
+   (convertedUnit === "celsius" && convertedTemp > 0 && convertedTemp < 26.67) ||
+   (convertedUnit === "kelvin" && convertedTemp > 273.15  && convertedTemp < 299.82)) {
+   document.body.style.backgroundColor = "#f0f0f0";
+   mainContainer.style.backgroundColor = "#fff";
+   btn.style.backgroundColor = "#4CAF50"
+  }
 
 
+}
+
+function hot(convertedUnit, convertedTemp) {
+  var mainContainer = document.querySelector(".main-container");
+  var btn = document.querySelector(".btn");
+
+  if ((convertedUnit === "fahrenheit" && convertedTemp >= 80) ||
+   (convertedUnit === "celsius" && convertedTemp >= 26.67) ||
+   (convertedUnit === "kelvin" && convertedTemp >= 299.82)) {
+   document.body.style.backgroundColor = "#900c3f";
+   mainContainer.style.backgroundColor = "#c70039";
+   btn.style.backgroundColor = "#f94c10"
+  }
 }
